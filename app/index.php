4d01d5a625b58dcf40190234c6d5209ee17501b6
njarach <101182825+njarach@users.noreply.github.com>
2024-04-17 12:01:03 +0200

    + ADDED DatabaseConnection.php with logic to connect to database, + CHANGED index.php to include DatabaseConnection.php, + ADDED .gitignore to ignore files from IDE

--- app/index.php
+++ app/index.php
@@ -1,5 +1,15 @@
 <?php
 
-// if / else logic here
+use src\DatabaseConnection;
 
-include 'controller/homePage.php';
+require_once 'src/DatabaseConnection.php';
+
+$host = 'localhost';
+$dbname = 'php_blog_adventure';
+$username = 'root';
+$password = '';
+
+// Connect to the database
+DatabaseConnection::connect($host,$dbname,$username,$password);
+
+// Routing to the stuff the user wants to do
